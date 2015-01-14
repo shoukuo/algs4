5c56dd96200175b73083dc660422dac7b0a7c50b
gengshoukuo <798633214@163.com>
2015-01-14 16:10:23 +0800

    algs4 ps2: 1st version

---
+++ ps2/Subset.java
@@ -0,0 +1,17 @@
+
+public class Subset {
+    public static void main(String[] args)
+    {
+        int k = Integer.parseInt(args[0]);
+        //int c = 0;
+        RandomizedQueue<String> rq = new RandomizedQueue<String>();
+        
+        while (!StdIn.isEmpty())
+        {
+            rq.enqueue(StdIn.readString());
+        }
+        
+        for (int i = 0; i < k; i++)
+            System.out.println(rq.dequeue());
+    }
+}
